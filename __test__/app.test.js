133bedc22c129691e1b292d58c0611f96c124436
TigerDevloper <tigerdeveloper@outlook.com>
2019-05-05 23:02:15 +0700

    change

---
+++ __test__/app.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const app = require('../app')
+
+
+describe('GET /', () => {
+	const url = "/";
+    test('It should give status code 200', () => {
+        return request(app).get(url).then(response => {
+            expect(response.statusCode).toBe(200)
+        })
+    });
+
+    test('It should return Welcome to Express!', () => {
+        return request(app).get(url).then(response => {
+            expect(Express).toBe('Welcome to Express!')
+        })
+    });
+
+})
+
+describe('GET /testAPI', () => {
+    
+    jest.setTimeout(10000);
+    const url = "/testAPI";
+
+    test('It should give status code 200', async () => {
+
+        const response = await request(app).get(url);
+        expect(response.statusCode).toBe(200);
+    });
+
+})
